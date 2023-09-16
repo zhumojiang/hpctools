3aa27e80df940d4d3f930a5733a3edbe7d40e4e3
Emilio J. Padrón González <emilioj@udc.gal>
2023-09-16 03:21:03 +0200

    Default compilation with GCC and Openblas

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 //#include <lapacke.h>
-//#include <openblas/lapacke.h>
-#include <mkl_lapacke.h>
+#include <openblas/lapacke.h>
+//#include <mkl_lapacke.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
